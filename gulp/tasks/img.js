7a3f4a517815adb335b8eeb533fa11db53448460
efekta <efekta9@gmail.com>
2020-11-30 16:50:56 +0300

    favicon

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     $.gulp.task('img', function() {
-        return $.gulp.src('src/static/img/*.{png,jpg,gif,svg,webp}')
+        return $.gulp.src('src/static/img/*.{png,jpg,gif,svg,ico,webp}')
             .pipe($.gulp.dest('build/img'))
     });
 }
